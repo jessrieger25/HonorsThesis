c910e031cfd652654073a5531458eeac21ba28d2
jessrieger25 <Jess@Jessicas-MacBook-Pro-2.local>
2018-10-17 22:03:55 -0400

    Testing batch

--- models/analysis_drivers.py
+++ models/analysis_drivers.py
@@ -154,11 +154,11 @@ class AnalysisDriver():
 # AnalysisDriver(["os.path.abspath("../time_machine_used/time_machine_skip_gram.txt")])
 
 AnalysisDriver([os.path.abspath("../ficino/book_1_part_1.txt"),
-                os.path.abspath("../ficino/book_2.txt")])
-                # os.path.abspath("../ficino/book_5.txt"),
-                # os.path.abspath("../ficino/book_6.txt"),
-                # os.path.abspath("../ficino/book_9_and_10.txt"),
-                # os.path.abspath("../ficino/book_12.txt")])
+                os.path.abspath("../ficino/book_2.txt"),
+                os.path.abspath("../ficino/book_5.txt"),
+                os.path.abspath("../ficino/book_6.txt"),
+                os.path.abspath("../ficino/book_9_and_10.txt"),
+                os.path.abspath("../ficino/book_12.txt")])
 
 
 
